0173838d3df4729a0ff56cd2cfd22f3e223d41a6
Rodmar Zavala <rodmar.zavala@pragawebstudio.com>
2016-04-14 21:44:07 -0600

    Filtering data with publish and subscribe

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -1,9 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-
 import { Tasks } from '../api/tasks.js';
-
 import './task.html';
+
+Template.task.helpers({
+    isOwner() {
+        return this.owner === Meteor.userId();
+    },
+});
 
 Template.task.events({
     'click .toggle-checked'() {
@@ -12,5 +16,8 @@ Template.task.events({
     },
     'click .delete'() {
         Meteor.call('tasks.remove', this._id);
+    },
+    'click .toggle-private'() {
+        Meteor.call('tasks.setPrivate', this._id, !this.private);
     },
 });
